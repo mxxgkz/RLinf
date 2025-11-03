b0719133353c228aed86da19698c8564ebce593f
mxxgkz <kz29duke@gmail.com>
2025-11-03 01:42:04 -0600

    Edit yaml file and add slurm script.

---
+++ script/clusters/quest/ft_batch.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+while IFS=$' ' read IDX HOUR CORE MEM CFG_NAME
+do
+STD_OUTPUT_FILE="/projects/p30309/RL/RLinf/script/clusters/std_output/${IDX}.log"
+
+JOB=`sbatch << EOJ
+#!/bin/bash
+#SBATCH -J ${IDX}
+#SBATCH -A p30309
+#SBATCH -p gengpu
+#SBATCH -t ${HOUR}:59:59
+#SBATCH --gres=gpu:2
+#SBATCH --mem=${MEM}G
+#SBATCH --cpus-per-task=${CORE}
+#SBATCH --output=${STD_OUTPUT_FILE}
+#SBATCH --mail-type=BEGIN,FAIL,END,REQUEUE #BEGIN,END,FAIL,REQUEUE
+#SBATCH --mail-user=zkghhg@gmail.com
+
+#Delete any preceding space after 'EOJ'. OW, there will be some error.
+
+# unload any modules that carried over from your command line session
+module purge
+
+# Set your working directory
+cd /projects/p30309/RL/RLinf/   #$PBS_O_WORKDIR
+
+# load modules you need to use
+# module load python/anaconda3.6
+
+conda init bash
+
+which conda
+
+source .venv/bin/activate
+
+export LD_LIBRARY_PATH="/home/kzy816/.mujoco/mujoco210/bin:/usr/lib/nvidia:\$LD_LIBRARY_PATH"
+export DPPO_DATA_DIR="/projects/p30309/RL/RLinf/data"
+export DPPO_LOG_DIR="/projects/p30309/RL/RLinf/log"
+
+echo "=== Debug LD_LIBRARY_PATH ==="
+echo "LD_LIBRARY_PATH: \$LD_LIBRARY_PATH"
+echo "Python path: \$(which python)"
+
+# # A command you actually want to execute:
+# java -jar <someinput> <someoutput>
+# # Another command you actually want to execute, if needed:
+# python myscript.py
+
+which python
+
+bash examples/embodiment/run_embodiment.sh ${CFG_NAME}
+
+EOJ
+`
+
+# print out the job id for reference later
+echo "JobID = ${JOB} for indices ${IDX} and parameters ${CFG_NAME} submitted on `date`"
+
+sleep 0.5
+
+done < /projects/p30309/RL/RLinf/script/clusters/quest/param.info
+# done < ./command_script/param_unet_exp.info
+# done < ./command_script/param_trex_online.info
+# done < ./command_script/param_ar_lin_2d.info
+exit
+
+# make this file executable and then run from the command line
+# chmod u+x submit.sh
+# ./submit.sh
+# The last line of params.txt have to be an empty line.
